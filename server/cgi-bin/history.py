b819b25c6d67cb8bf02431323c2642f72ff4122e
pingu342 <pingu342@users.noreply.github.com>
2023-04-19 13:27:56 +0000

    history

--- server/cgi-bin/history.py
+++ server/cgi-bin/history.py
@@ -33,22 +33,31 @@ prv = python_bitbankcc.private(API_KEY, API_SECRET)
 
 print('Content-type: text/html; charset=UTF-8\r\n')
 
-#orders = prv.get_active_orders(PAIR)
-#for order in orders['orders']:
-#    if matching_order(DIR + 'orders.txt', order['order_id']):
+print('order', '<br/>')
+orders = prv.get_active_orders(PAIR)
+for order in orders['orders']:
+    if matching_order(DIR + 'orders.txt', order['order_id']):
+        a = order['start_amount']
+        p = order['price']
+        r = int(float(a) * float(p))
+        t = time.localtime(int(order['ordered_at'])/1000 + 60*60*9)
+        print(order['order_id'], ',',
+                time.strftime("%Y-%m-%d %H:%M:%S", t), ',',
+                a, ',', p, ',', r, '<br/>')
 
 value = prv.get_trade_history(PAIR, 1000)
 
 # print(json.dumps(value))
 # {"trade_id": 3969372, "order_id": 324460818, "pair": "btc_jpy", "side": "buy", "type": "limit", "amount": "0.0050", "price": "2000000", "maker_taker": "maker", "fee_amount_base": "0.00000000", "fee_amount_quote": "0.0000", "executed_at": 1513733421000}
 
+print('history', '<br/>')
 for trade in value['trades']:
     if matching_order(DIR + 'orders.txt', trade['order_id']):
         a = trade['amount']
         p = trade['price']
         r = int(float(a) * float(p))
-        t = time.localtime(int(trade['executed_at'])/1000)
+        t = time.localtime(int(trade['executed_at'])/1000 + 60*60*9)
         print(trade['order_id'], ',',
-                time.strftime("%Y/%m/%d %H:%M:%S", t), ',',
+                time.strftime("%Y-%m-%d %H:%M:%S", t), ',',
                 a, ',', p, ',', r, '<br/>')
 
