a5fa1b02d2d87cfa567f40d237517386b45c5757
pingu342 <pingu342@users.noreply.github.com>
2023-05-03 09:33:35 +0000

    layout fix

--- server/cgi-bin/get_info.py
+++ server/cgi-bin/get_info.py
@@ -9,9 +9,9 @@ from util import save_current_time, calculate_time_diff
 
 DIR = join(dirname(__file__), '../../')
 
-hostname = os.environ.get("APP_HIDDEN_SERVICE")
-if hostname == None:
-    hostname = ''
+tor_name = os.environ.get("APP_HIDDEN_SERVICE")
+if tor_name == None:
+    tor_name = 'not available'
 
 with open(DIR + 'config.yaml', 'r') as f:
     data = yaml.safe_load(f)
@@ -24,7 +24,7 @@ print('Content-type: application/json\r\n')
 if new_order:
     remain = int(interval - calculate_time_diff(DIR + 'time.txt'))
     td = datetime.timedelta(seconds=remain)
-    print('{"new_order": true, "remain": "%s", "hostname": "%s"}\n' % (td, hostname))
+    print('{"new_order": true, "remain": "%s", "tor_name": "%s"}\n' % (td, tor_name))
 else:
-    print('{"new_order": false, "remain": -1, "hostname": "%s"}\n' % hostname)
+    print('{"new_order": false, "remain": -1, "tor_name": "%s"}\n' % tor_name)
 
