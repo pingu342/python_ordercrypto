2fa86da7bb6a3b558508661261ba77f7a8424491
pingu342 <pingu342@users.noreply.github.com>
2023-05-03 10:18:23 +0000

    minor fix

--- server/cgi-bin/enable_new_order.py
+++ server/cgi-bin/enable_new_order.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys
+import cgi
 import yaml
 from os.path import join, dirname
 
@@ -8,9 +9,15 @@ DIR = join(dirname(__file__), '../../')
 with open(DIR + 'config.yaml', 'r') as f:
     config = yaml.safe_load(f)
 
-print(config)
+new_order = True
 
-config['settings']['new_order'] = True
+form = cgi.FieldStorage()
+if 'disable' in form:
+    value = form.getvalue('disable')
+    if value == '1':
+        new_order = False
+
+config['settings']['new_order'] = new_order
 
 with open(DIR + 'config.yaml', 'w') as f:
     yaml.safe_dump(config, f)
